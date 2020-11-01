a41711f38237a799b073195e129b740634920689
Kevin Van Leer <kevin.vanleer@gmail.com>
2020-11-01 06:31:45 -0600

    Added Heading component

---
+++ app/src/main/java/com/kvl/cyclotrack/Heading.kt
@@ -0,0 +1,45 @@
+package com.kvl.cyclotrack
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class Heading(context: Context, attrs: AttributeSet) : ConstraintLayout(context, attrs) {
+
+    private var labelAttr = "LABEL"
+    private lateinit var headingLabelView: TextView
+    private lateinit var headingValueView: TextView
+
+    var label: CharSequence
+        get() = headingLabelView.text
+        set(newValue) {
+            headingLabelView.text
+        }
+
+    var value: CharSequence
+        get() = headingValueView.text
+        set(newValue) {
+            headingValueView.text
+        }
+
+
+    init {
+        View.inflate(context, R.layout.heading, this)
+
+        val attributes = context.obtainStyledAttributes(attrs, R.styleable.Heading, 0, 0)
+
+        labelAttr = attributes.getString(R.styleable.Heading_label) ?: ""
+
+        attributes.recycle()
+    }
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        headingLabelView = findViewById(R.id.heading_label)
+        headingValueView = findViewById(R.id.heading_value)
+
+        label = labelAttr
+    }
+}
