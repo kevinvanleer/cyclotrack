1e5290c3aeb06c008ee2312d2892df83d01a0f93
Kevin Van Leer <kevin.vanleer@gmail.com>
2020-12-03 15:36:30 -0600

    Updated permissions request workflow for compliance

--- app/src/main/java/com/kvl/cyclotrack/MainActivity.kt
+++ app/src/main/java/com/kvl/cyclotrack/MainActivity.kt
@@ -1,50 +1,16 @@
 package com.kvl.cyclotrack
 
-import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    private fun initializeLocationService() {
-        when (PackageManager.PERMISSION_GRANTED) {
-            ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ),
-            -> {
-                // You can use the API that requires the permission.
-                //performAction(...)
-            }
-            /*
-            shouldShowRequestPermissionRationale(...) -> {
-            // In an educational UI, explain to the user why your app requires this
-            // permission for a specific feature to behave as expected. In this UI,
-            // include a "cancel" or "no thanks" button that allows the user to
-            // continue using your app without granting the permission.
-            showInContextUI(...)
-        }*/
-            else -> {
-                // You can directly ask for the permission.
-                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
-                    requestPermissions(
-                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,
-                            Manifest.permission.ACCESS_BACKGROUND_LOCATION),
-                        0)
-                }
-            }
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
-        initializeLocationService()
     }
 
     /*
@@ -58,34 +24,6 @@ class MainActivity : AppCompatActivity() {
     }
     */
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>, grantResults: IntArray,
-    ) {
-        when (requestCode) {
-            0 -> {
-                // If request is cancelled, the result arrays are empty.
-                if ((grantResults.isNotEmpty() &&
-                            grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                ) {
-                    //TODO: SOMETHING WHEN PERMISSION IS GRANTED
-                } else {
-                    // Explain to the user that the feature is unavailable because
-                    // the features requires a permission that the user has denied.
-                    // At the same time, respect the user's decision. Don't link to
-                    // system settings in an effort to convince the user to change
-                    // their decision.
-                }
-                return
-            }
-
-            // Add other 'when' lines to check for other
-            // permissions this app might request.
-            else -> {
-                // Ignore all other requests.
-            }
-        }
-    }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
--- app/src/main/java/com/kvl/cyclotrack/TripSummariesFragment.kt
+++ app/src/main/java/com/kvl/cyclotrack/TripSummariesFragment.kt
@@ -1,8 +1,12 @@
 package com.kvl.cyclotrack
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.navGraphViewModels
@@ -15,6 +19,117 @@ import dagger.hilt.android.AndroidEntryPoint
 class TripSummariesFragment : Fragment() {
     private val viewModel: TripSummariesViewModel by navGraphViewModels(R.id.cyclotrack_nav_graph) {
         defaultViewModelProviderFactory
+    }
+
+    private var alertDialog: AlertDialog? = null
+
+    private fun initializeLocationService() {
+        /*when (PackageManager.PERMISSION_GRANTED) {
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            -> {
+                // You can use the API that requires the permission.
+                //performAction(...)
+                findNavController().navigate(R.id.action_start_trip)
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION),
+            -> {
+                // In an educational UI, explain to the user why your app requires this
+                // permission for a specific feature to behave as expected. In this UI,
+                // include a "cancel" or "no thanks" button that allows the user to
+                // continue using your app without granting the permission.
+                showInContextUI(...)
+            }
+            else -> {
+                // You can directly ask for the permission.
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+                    requestPermissions(
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_BACKGROUND_LOCATION),
+                        0)
+                }
+            }
+        }*/
+        when {
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                findNavController().navigate(R.id.action_start_trip)
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+                val eduDialog: AlertDialog? = activity?.let {
+                    val builder = AlertDialog.Builder(it)
+                    builder.apply {
+                        setPositiveButton("ALLOW"
+                        ) { dialog, id ->
+                            // User clicked OK button
+                            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+                                requestPermissions(
+                                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,
+                                        Manifest.permission.ACCESS_BACKGROUND_LOCATION),
+                                    0)
+                            }
+                        }
+                        setNegativeButton("DENY"
+                        ) { dialog, id ->
+                            // User cancelled the dialog
+                            Log.d("TRIP_SUMMARIES", "CLICKED DENY")
+                            alertDialog?.show()
+                        }
+                        setTitle("Grant Location Access")
+                        setMessage("Cyclotrack needs access to GPS location data and background location data to calculate speed and distance traveled during your rides. Data is only recorded while rides are in progress. Data collected is stored on your device for your personal use. Your data is not sent to any third parties, including the developer. Please select ALLOW and then grant Cyclotrack access to location data.")
+                    }
+
+                    builder.create()
+                }
+                eduDialog?.show()
+            }
+            else -> {
+                // You can directly ask for the permission.
+                // The registered ActivityResultCallback gets the result of this request.
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+                    requestPermissions(
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_BACKGROUND_LOCATION),
+                        0)
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray,
+    ) {
+        when (requestCode) {
+            0 -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() &&
+                            grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                ) {
+                    //TODO: SOMETHING WHEN PERMISSION IS GRANTED
+                    findNavController().navigate(R.id.action_start_trip)
+                } else {
+                    // Explain to the user that the feature is unavailable because
+                    // the features requires a permission that the user has denied.
+                    // At the same time, respect the user's decision. Don't link to
+                    // system settings in an effort to convince the user to change
+                    // their decision.
+
+                    alertDialog?.show()
+                }
+                return
+            }
+
+            // Add other 'when' lines to check for other
+            // permissions this app might request.
+            else -> {
+                // Ignore all other requests.
+            }
+        }
     }
 
     override fun onCreateView(
@@ -43,7 +158,20 @@ class TripSummariesFragment : Fragment() {
         })
 
         view.findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
-            findNavController().navigate(R.id.action_start_trip)
+            initializeLocationService()
+        }
+        alertDialog = activity?.let {
+            val builder = AlertDialog.Builder(it)
+            builder.apply {
+                setPositiveButton("OK"
+                ) { _, _ ->
+                    Log.d("ALERT DIALOG", "CLICKED")
+                }
+                setTitle("New rides disabled")
+                setMessage("Please access Cyclotrack app settings and grant permission to Location data. You will not be able to start a ride until location permission is granted.\n\nCyclotrack needs access to GPS location data and background location data to calculate speed and distance traveled during your rides. Data is only recorded while rides are in progress. Data collected is stored on your device for your personal use. Your data is not sent to any third parties, including the developer.")
+            }
+
+            builder.create()
         }
     }
 
