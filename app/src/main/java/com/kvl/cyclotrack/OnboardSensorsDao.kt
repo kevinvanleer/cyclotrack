30a0524d8bad00df02d0606ac1844cdec0b78730
Kevin Van Leer <kevin.vanleer@gmail.com>
2021-03-27 06:41:14 -0500

    Added load query to onboard sensor DAO

--- app/src/main/java/com/kvl/cyclotrack/OnboardSensorsDao.kt
+++ app/src/main/java/com/kvl/cyclotrack/OnboardSensorsDao.kt
@@ -1,11 +1,16 @@
 package com.kvl.cyclotrack
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
 interface OnboardSensorsDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun save(measurements: OnboardSensors): Long
+
+    @Query("SELECT * FROM OnboardSensors WHERE tripId = :tripId")
+    fun load(tripId: Long): LiveData<Array<OnboardSensors>>
 }
--- app/src/main/java/com/kvl/cyclotrack/OnboardSensorsRepository.kt
+++ app/src/main/java/com/kvl/cyclotrack/OnboardSensorsRepository.kt
@@ -5,4 +5,6 @@ import javax.inject.Inject
 class OnboardSensorsRepository @Inject constructor(private val onboardSensorsDao: OnboardSensorsDao) {
     suspend fun insertMeasurements(tripId: Long, measurements: SensorModel) =
         onboardSensorsDao.save(OnboardSensors(tripId, measurements))
+
+    fun getTripMeasurements(tripId: Long) = onboardSensorsDao.load(tripId)
 }
