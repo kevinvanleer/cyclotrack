37baea4d51c02dbbb1151d2d3dffbdfe6ce1ff71
Kevin Van Leer <kevin.vanleer@gmail.com>
2021-11-07 11:52:01 -0600

    wip

--- app/src/main/java/com/kvl/cyclotrack/Trip.kt
+++ app/src/main/java/com/kvl/cyclotrack/Trip.kt
@@ -83,7 +83,7 @@ data class Trip(
     val averageSpeed: Float? = null,
     val timestamp: Long = SystemUtils.currentTimeMillis(),
     val inProgress: Boolean = true,
-    val bikeId: Long? = null,
+    val bikeId: Long,
     @PrimaryKey(autoGenerate = true)
     val id: Long? = null,
     val notes: String? = null,
