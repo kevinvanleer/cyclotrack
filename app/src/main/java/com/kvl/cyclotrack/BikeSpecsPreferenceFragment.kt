e9e3012984d5e74d33c0506aac03d704d5130fa6
Kevin Van Leer <kevin.vanleer@gmail.com>
2021-11-13 06:03:20 -0600

    wip

--- app/src/main/java/com/kvl/cyclotrack/BikeSpecsPreferenceFragment.kt
+++ app/src/main/java/com/kvl/cyclotrack/BikeSpecsPreferenceFragment.kt
@@ -1,17 +1,23 @@
 package com.kvl.cyclotrack
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
+import android.widget.DatePicker
 import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
-import androidx.preference.PreferenceManager
 import com.kvl.cyclotrack.databinding.FragmentBikeSpecsPreferenceBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
 
 @AndroidEntryPoint
 class BikeSpecsPreferenceFragment : Fragment() {
@@ -19,19 +25,62 @@ class BikeSpecsPreferenceFragment : Fragment() {
         fun newInstance() = BikeSpecsPreferenceFragment()
     }
 
-    private lateinit var viewModel: BikeSpecsPreferenceViewModel
+    private val viewModel: BikeSpecsPreferenceViewModel by viewModels()
     private lateinit var binding: FragmentBikeSpecsPreferenceBinding
+
+    private fun initializePurchaseDate() {
+        binding.preferencePreferenceBikeSpecsPurchaseDate.setOnClickListener {
+            val purchaseDate = viewModel.purchaseDate?.let {
+                try {
+                    viewModel.purchaseDate?.let {
+                        SimpleDateFormat(
+                            dateFormatPattenDob,
+                            Locale.US
+                        ).parse(it)
+                    }
+                } catch (e: ParseException) {
+                    Log.e(tag, "Could not parse preference data", e)
+                    null
+                }
+            }
+
+            val default = GregorianCalendar.getInstance()
+            val c = GregorianCalendar()
+            c.time = purchaseDate ?: default.time
+            val year = c.get(GregorianCalendar.YEAR)
+            val month = c.get(GregorianCalendar.MONTH)
+            val day = c.get(GregorianCalendar.DAY_OF_MONTH)
+
+            val datePicker = DatePicker(context)
+            datePicker.updateDate(year, month, day)
+            context?.let { thisContext ->
+                AlertDialog.Builder(thisContext).apply {
+                    setTitle("Date of birth")
+                    setView(datePicker)
+                    setPositiveButton("OK") { _, _ ->
+                        val newDate = GregorianCalendar.getInstance()
+                        newDate.set(datePicker.year, datePicker.month, datePicker.dayOfMonth)
+                        viewModel.purchaseDate =
+                            SimpleDateFormat(dateFormatPattenDob).format(
+                                newDate.time
+                            )
+                        binding.preferencePreferenceBikeSpecsPurchaseDate.setText(viewModel.purchaseDate)
+                    }
+                }.create().show()
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        viewModel =
+        /*viewModel =
             BikeSpecsPreferenceViewModel(
                 PreferenceManager.getDefaultSharedPreferences(
                     requireContext()
                 )
-            )
+            )*/
         binding = FragmentBikeSpecsPreferenceBinding.inflate(
             inflater,
             container,
@@ -40,6 +89,8 @@ class BikeSpecsPreferenceFragment : Fragment() {
         binding.viewmodel = viewModel
 
         activity?.title = "Settings: Bike specs"
+
+        initializePurchaseDate()
 
         return binding.root
     }
--- app/src/main/java/com/kvl/cyclotrack/BikeSpecsPreferenceViewModel.kt
+++ app/src/main/java/com/kvl/cyclotrack/BikeSpecsPreferenceViewModel.kt
@@ -2,7 +2,6 @@ package com.kvl.cyclotrack
 
 import android.content.SharedPreferences
 import androidx.core.content.edit
-import androidx.databinding.Bindable
 import androidx.lifecycle.ViewModel
 import com.kvl.cyclotrack.repos.BikeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -16,7 +15,6 @@ class BikeSpecsPreferenceViewModel @Inject constructor(
     private val logTag = "BikeSpecsViewModel"
 
     var circumference
-        @Bindable
         get() =
             sharedPreferences.getString(
                 CyclotrackApp.instance.getString(R.string.preference_key_wheel_circumference),
@@ -32,7 +30,6 @@ class BikeSpecsPreferenceViewModel @Inject constructor(
         }
 
     var useAutoCircumference: Boolean
-        @Bindable
         get() =
             try {
                 sharedPreferences.getBoolean(CyclotrackApp.instance.getString(R.string.preference_key_useAutoCircumference),
@@ -50,7 +47,6 @@ class BikeSpecsPreferenceViewModel @Inject constructor(
         }
 
     var bikeMass
-        @Bindable
         get() =
             sharedPreferences.getString(
                 CyclotrackApp.instance.getString(R.string.preference_key_bike_mass),
@@ -66,7 +62,6 @@ class BikeSpecsPreferenceViewModel @Inject constructor(
         }
 
     var purchaseDate
-        @Bindable
         get() =
             sharedPreferences.getString(
                 CyclotrackApp.instance.getString(R.string.preference_key_bike_mass),
@@ -81,11 +76,9 @@ class BikeSpecsPreferenceViewModel @Inject constructor(
             }
         }
 
-    @get:Bindable
     val circumferenceHint: String
         get() = "Wheel circumference (in or mm)"
 
-    @get:Bindable
     val bikeMassHint: String
         get() =
             when (sharedPreferences.getString(CyclotrackApp.instance.getString(R.string.preference_key_system_of_measurement),
@@ -94,7 +87,6 @@ class BikeSpecsPreferenceViewModel @Inject constructor(
                 else -> "Bike weight (kg)"
             }
 
-    @get:Bindable
     val instructionText: String
         get() = "Cyclotrack uses your bike's specs along with sensor data to calculate speed and other metrics. BLE speed sensor required to utilize wheel circumference."
 
