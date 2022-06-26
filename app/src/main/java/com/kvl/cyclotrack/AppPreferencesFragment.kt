6a4f1130daae17508d119e1dbf267fbb56bed642
Kevin Van Leer <kevin.vanleer@gmail.com>
2022-06-26 17:05:31 -0500

    wip

--- app/src/main/java/com/kvl/cyclotrack/AppPreferencesFragment.kt
+++ app/src/main/java/com/kvl/cyclotrack/AppPreferencesFragment.kt
@@ -34,14 +34,12 @@ class AppPreferencesFragment : PreferenceFragmentCompat() {
     init {
         Log.d(logTag, "register for strava-auth result")
         Log.d(logTag, "$this")
-        Log.d(logTag, "${this.id}")
     }
 
     private val activityResultLauncher =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             Log.d(logTag, "received activity result")
             Log.d(logTag, "$this")
-            Log.d(logTag, "${this.id}")
             Log.d(logTag, "$result")
             if (result.resultCode == Activity.RESULT_OK) {
                 Log.d(logTag, "user authorized access to strava")
@@ -291,6 +289,5 @@ class AppPreferencesFragment : PreferenceFragmentCompat() {
         super.onResume()
         requireActivity().findViewById<Toolbar>(R.id.preferences_toolbar).title = "Settings"
         Log.d(logTag, "$this")
-        Log.d(logTag, "${this.id}")
     }
 }
