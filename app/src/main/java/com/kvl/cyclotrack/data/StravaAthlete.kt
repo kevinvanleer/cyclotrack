62926ccae0e7612665882135291f4d8d380c4b6e
Kevin Van Leer <kevin.vanleer@gmail.com>
2022-06-28 09:05:50 -0500

    Storing access token in shared prefs

--- app/src/main/java/com/kvl/cyclotrack/data/StravaAthlete.kt
+++ app/src/main/java/com/kvl/cyclotrack/data/StravaAthlete.kt
@@ -5,9 +5,9 @@ import androidx.annotation.Keep
 @Keep
 data class StravaAthlete(
     val id: Long,
-    val firstname: String,
-    val lastname: String,
-    val sex: String,
-    val ftp: Integer,
-    val weight: Float
+    val firstname: String? = null,
+    val lastname: String? = null,
+    val sex: String? = null,
+    val ftp: Integer? = null,
+    val weight: Float? = null,
 )
