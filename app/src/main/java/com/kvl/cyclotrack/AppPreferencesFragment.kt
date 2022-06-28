249816f509e9b2fec27ac40c3af418344559a6f9
Kevin Van Leer <kevin.vanleer@gmail.com>
2022-06-28 07:26:35 -0500

    wip

--- app/src/main/java/com/kvl/cyclotrack/AppPreferencesFragment.kt
+++ app/src/main/java/com/kvl/cyclotrack/AppPreferencesFragment.kt
@@ -3,6 +3,7 @@ package com.kvl.cyclotrack
 import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
