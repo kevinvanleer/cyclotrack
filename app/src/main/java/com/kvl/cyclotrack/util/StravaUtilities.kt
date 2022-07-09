17406ca46234b24a7d7fafa07a2fab40a304a8cf
Kevin Van Leer <kevin.vanleer@gmail.com>
2022-07-09 15:29:02 -0500

    wip

--- app/src/main/java/com/kvl/cyclotrack/util/StravaUtilities.kt
+++ app/src/main/java/com/kvl/cyclotrack/util/StravaUtilities.kt
@@ -195,12 +195,13 @@ fun syncTripWithStrava(
         messages
     )
 
+    Log.d(logTag, "validAccessToken=$validAccessToken")
     try {
         validAccessToken ?: getPreferences(appContext).getString(
             appContext.getString(R.string.preference_key_strava_access_token),
             null
         )?.let { accessToken ->
-            Log.d(logTag, "Sending activity")
+            Log.d(logTag, "Sending activity: $accessToken")
             return sendActivityToStrava(accessToken, privateAppFile, exportData.summary!!).also {
                 privateAppFile.delete()
             }
