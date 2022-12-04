f5aef9589781644a2bfcf151f1b197aacfe311be
Kevin Van Leer <kevin.vanleer@gmail.com>
2022-12-04 15:45:46 -0600

    wip

--- app/src/test/java/com/kvl/cyclotrack/GeometryTest.kt
+++ app/src/test/java/com/kvl/cyclotrack/GeometryTest.kt
@@ -8,6 +8,7 @@ import kotlin.math.PI
 class GeometryTest {
     @Test
     fun rotate_basic() {
-        Assert.assertEquals(Pair(-1f, -1f), rotate((PI / 2).toFloat(), Pair(1f, 1f), Pair(0f, 0f)))
+        val result = rotate((PI / 2).toFloat(), Pair(1f, 1f), Pair(0f, 0f))
+        Assert.assertEquals(-1f, result.first, 0.001f)
     }
 }
