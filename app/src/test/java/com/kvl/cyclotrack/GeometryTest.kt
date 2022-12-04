c669e051a3fa052ce59c096f60b35189b0b81bd6
Kevin Van Leer <kevin.vanleer@gmail.com>
2022-12-04 16:56:51 -0600

    wip

--- app/src/test/java/com/kvl/cyclotrack/GeometryTest.kt
+++ app/src/test/java/com/kvl/cyclotrack/GeometryTest.kt
@@ -19,5 +19,9 @@ class GeometryTest {
         result = rotate((PI / 2).toFloat(), Pair(1f, 1f), Pair(1f, 0f))
         Assert.assertEquals(0f, result.first, 0.001f)
         Assert.assertEquals(0f, result.second, 0.001f)
+
+        result = rotate((PI / 2).toFloat(), Pair(0.6f, 0.8f), Pair(0.1f, 0.7f))
+        Assert.assertEquals(0f, result.first, 0.001f)
+        Assert.assertEquals(1.4f, result.second, 0.001f)
     }
 }
