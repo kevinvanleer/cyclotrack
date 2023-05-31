7d7d8b5349e389c2cc7f6002a625d82ce40cf331
Kevin Van Leer <kevin.vanleer@gmail.com>
2023-05-31 16:12:54 -0500

    Stop trip in progress if deleted

--- app/src/main/java/com/kvl/cyclotrack/RemoveTripWorker.kt
+++ app/src/main/java/com/kvl/cyclotrack/RemoveTripWorker.kt
@@ -1,6 +1,7 @@
 package com.kvl.cyclotrack
 
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
@@ -22,6 +23,21 @@ class RemoveTripWorker @AssistedInject constructor(
     override suspend fun doWork(): Result {
         inputData.getLongArray("tripIds")?.takeIf { it.isNotEmpty() }?.forEach { tripId ->
             Log.i(logTag, "Removing data from dB for trip $tripId")
+            tripsRepository.get(tripId).let { trip ->
+                trip.takeIf { trip.inProgress }?.let { tripInProgress ->
+                    applicationContext.stopService(
+                        Intent(
+                            applicationContext,
+                            TripInProgressService::class.java
+                        ).apply {
+                            this.action =
+                                applicationContext.getString(R.string.action_stop_trip_service)
+                            this.putExtra("tripId", tripInProgress.id)
+                        })
+
+                }
+
+            }
             tripsRepository.removeTrip(tripId)
         }
         return Result.success()
