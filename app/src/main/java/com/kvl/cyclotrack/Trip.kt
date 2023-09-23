5e2994e8483d26c1178d78b95b05eb83b9a207d7
Kevin Van Leer <kevin.vanleer@gmail.com>
2023-09-23 09:59:58 -0500

    Make trip fields non-nullable

--- app/src/main/java/com/kvl/cyclotrack/Trip.kt
+++ app/src/main/java/com/kvl/cyclotrack/Trip.kt
@@ -1,9 +1,13 @@
 package com.kvl.cyclotrack
 
 import androidx.annotation.Keep
-import androidx.room.*
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverter
 import com.kvl.cyclotrack.util.SystemUtils
-import java.util.*
+import java.util.Calendar
 
 enum class GoogleFitSyncStatusEnum(val value: Int) {
     NOT_SYNCED(0),
@@ -78,15 +82,15 @@ fun getDefaultTripName(): String {
 )
 @Keep
 data class Trip(
-    val name: String? = getDefaultTripName(),
-    val distance: Double? = 0.0,
-    val duration: Double? = null,
-    val averageSpeed: Float? = null,
+    val name: String = getDefaultTripName(),
+    val distance: Double = 0.0,
+    val duration: Double = 0.0,
+    val averageSpeed: Float = 0f,
     val timestamp: Long = SystemUtils.currentTimeMillis(),
     val inProgress: Boolean = true,
     val bikeId: Long,
     @PrimaryKey(autoGenerate = true)
-    val id: Long? = null,
+    val id: Long = 0,
     val notes: String? = null,
     val userWheelCircumference: Float? = null,
     val autoWheelCircumference: Float? = null,
